ff26dca13467a083fad4a6c7ad76c36c5e4ba942
Guguuuu <Guguuuu@qq.com>
2022-07-24 00:35:23 +0800

    添加ColumnDetail组件的细节，展示专栏文章

--- src/pages/ColumnDetail.vue
+++ src/pages/ColumnDetail.vue
@@ -1,25 +1,37 @@
 <template>
-    <h1>
-        <!-- 对于一个Object来说,如果想在页面中显示它所有内容，除了js的console.log 还可以用pre标签 -->
-        <!-- pre标签可以定义预格式化的文本，在pre元素中，文本会保留空格、换行符，且文本显示等宽字体 -->
-        <pre>{{ route }}</pre>
-    </h1>
+    <div class="column-detail-page w-75 mx-auto">
+        <div class="column-info row mb-4 border-bottom pb-4 align-items-center" v-if="column">
+            <div class="col-3 text-center">
+                <img :src="column.avatar" :alt="column.title" class="rounded-circle border ">
+            </div>
+            <div class="col-9">
+                <h4>{{ column.title }}</h4>
+                <p class="text-muted">{{ column.description }}</p>
+            </div>
+        </div>
+        <post-list :list="list"></post-list>
+    </div>
 </template>
 
 <script lang="ts">
 /* eslint-disable */
-import { defineComponent } from "vue";
-//为了访问url的内容，需要一个钩子函数
-import { useRoute } from "vue-router";
+import { defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
+import { testData, testPosts } from '../testData'
+import PostList from '../components/PostList.vue'
 export default defineComponent({
+    components: {
+        PostList
+    },
     setup() {
         const route = useRoute()
+        const currentId = +route.params.id
+        const column = testData.find(c => c.id === currentId)
+        const list = testPosts.filter(post => post.columnId === currentId)
         return {
-            route
+            column,
+            list
         }
     }
 })
 </script>
-
-<style>
-</style>
