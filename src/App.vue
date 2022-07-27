d295fe1a9612f5ac0005c856f31a11b65d2762d2
Guguuuu <Guguuuu@qq.com>
2022-07-28 00:31:43 +0800

    完善Loader组件的编码2

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <!-- 查看这个Loader，可以发现这个组件是被包裹在container中间的，这种结构看起来会比较奇怪，应该把这个loding这个盒子提升到外面去，因为他是一个全局的效果 -->
-    <loader text="拼命加载中" background="rgba(0,0,0,0.8)"></loader>
+    <!-- 1.查看这个Loader，可以发现这个组件是被包裹在container中间的，这种结构看起来会比较奇怪，应该把这个loding这个盒子提升到外面去，因为他是一个全局的效果 -->
+    <loader v-if="isLoading"></loader>
     <router-view></router-view>
     <footer class="text-center py-4 text-secondary bg-light mt-6">
       <small>
--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="d-flex justify-content-center align-items-center h-100 loading-container"
-        :style="{ backgroundColor: background || '' }">
-        <div class="loading-content">
-            <div class="spinner-border text-primary" role="status">
-                <span class="visually-hidden">{{ text || 'Loading...' }}</span>
+    <teleport to="#back">
+        <!-- 2.但是呢，通过teleport在html中加上一个div并不是一个良好的设计，所以我们想使用另外一种方法来实现对应的功能。我们需要在body中创建一个最顶层的div。我们可以在组件还没完全挂载到DOM之前，手动创建一个div，然后把这个div的id设置成back就可以了。在生命周期中，setup的执行比beforecreate还要早，所以我们可以在setup中创建DOM节点 -->
+        <div class="d-flex justify-content-center align-items-center h-100 loading-container"
+            :style="{ backgroundColor: background || '' }">
+            <div class="loading-content">
+                <div class="spinner-border text-primary" role="status">
+                    <span class="visually-hidden">{{ text || 'Loading...' }}</span>
+                </div>
+                <p v-if="text" class="text-primary small">{{ text }}</p>
             </div>
-            <p v-if="text" class="text-primary small">{{ text }}</p>
         </div>
-    </div>
+    </teleport>
 </template>
 
 <script lang="ts">
 /* eslint-disable */
-import { defineComponent } from 'vue';
+import { defineComponent, onUnmounted } from 'vue';
 
 export default defineComponent({
     name: 'Loader',
@@ -24,6 +27,15 @@ export default defineComponent({
         background: {
             type: String
         }
+    },
+    setup() {
+        const node = document.createElement('div')
+        node.id = 'back'
+        document.body.appendChild(node)
+        onUnmounted(() => {
+            // 每次加载完成就把动态创建的用来展示loading的节点删除掉
+            document.body.removeChild(node)
+        })
     }
 })
 </script>
