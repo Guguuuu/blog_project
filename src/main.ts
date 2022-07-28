e4dc686305862ce59a270ba79528f75a1adadf67
Guguuuu <Guguuuu@qq.com>
2022-07-28 21:13:30 +0800

    8.1-登录的第一部分，获取token

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,19 @@ import App from './App.vue'
 
 axios.defaults.baseURL = 'http://apis.imooc.com/api/'
 axios.interceptors.request.use(config => {
-    config.params = { ...config.params, icode: '04D4AF3D4966A05E' }
+    // 其他代码
     store.commit('setLoading', true)
+
+    // get请求，添加到URL中
+    config.params = { ...config.params, icode: '04D4AF3D4966A05E' }
+    // 其他请求，添加到body中
+    // 如果是上传文件，添加到FormData中
+    if (config.data instanceof FormData) {
+        config.data.append('icode', '04D4AF3D4966A05E')
+    } else {
+        // 普通的body对象，添加到data中
+        config.data = { ...config.data, icode: '04D4AF3D4966A05E' }
+    }
     return config
 })
 axios.interceptors.response.use(config => {
